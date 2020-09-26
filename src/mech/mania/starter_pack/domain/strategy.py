09930fba1fb9cac73ad2ae109eacf6ad710d5029
Stone Tao <stonezt2019@gmail.com>
2020-09-25 22:23:10 -0700

    wtf

--- src/mech/mania/starter_pack/domain/strategy.py
+++ src/mech/mania/starter_pack/domain/strategy.py
@@ -84,7 +84,7 @@ class Strategy:
         self.logger.info("On board " + str(target_pos.board_id) +" move to (" + str(target_pos.x) + ", " + str(target_pos.y) + ")")
         decision = CharacterDecision(
             decision_type="MOVE",
-            action_position=target_pos,
+            action_position=target_pos.create(target_pos.x + 1, target_pos.y, target_pos.get_board_id()),
             action_index=0
         )
         self.logger.info("Moving!")
